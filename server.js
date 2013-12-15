38f95b0764522ba27f0281654c951851ff7543c3
jrubio <jose@twitchly.com>
2013-12-15 01:24:54 -0600

    Initial Restify setup

---
+++ server.js
@@ -0,0 +1,16 @@
+/**
+ * Created by jrubio on 12/15/13.
+ */
+var restify = require('restify');
+
+function respond(req, res, next) {
+    res.send('hello ' + req.params.name);
+}
+
+var server = restify.createServer();
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+server.listen(8080, function() {
+    console.log('%s listening at %s', server.name, server.url);
+});
